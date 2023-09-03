9520aea5c68ade3c3176c956107bf6fad463e29c
Edirisinghe E A M N <it21817694@my.sliit.lk>
2023-09-03 16:04:48 +0530

    Fuel Stock CRUD Operations added

--- backend/server.js
+++ backend/server.js
@@ -28,17 +28,22 @@ connection.once("open",() => {
     console.log("Your MongoDB connection is success!!!!!");
 })
 
-const dashboard = require("./routes/supplier")//dashbord
-const supplier = require("./routes/supplier.js")//supplier.js
+//const dashboard = require("./routes/supplier")//dashbord
 //const supplier = require("./routes/supplier.js")//supplier.js
+//const supplier = require("./routes/supplier.js")//supplier.js
+
+const fuel_entry = require("./routes/fuel_entry.js")
+const fuel_stock = require("./routes/fuel_stock.js")
 
 //another 8
 
 
 /*http://Localhost:8411/supplier*/
 
-app.use("/", dashboard);
-app.use("/supplier", supplier);
+//app.use("/", dashboard);
+//app.use("/supplier", supplier);
+app.use("/fuel_entry", fuel_entry);
+app.use("/fuel_stock", fuel_stock);
 //anothe 8
 
 app.listen(PORT, () =>{
