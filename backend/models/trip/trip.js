62d3647b1bf430f47c4759433b439946b5dc9220
Yohan Wickramasinghe <it21816086@my.sliit.lk>
2023-09-19 01:24:55 +0530

    Completing trip.js code inside models folder

---
+++ backend/models/trip/trip.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const tripSchema = new Schema({
+    
+    tripid : {
+        type: Number,
+        required: true
+    },
+
+    tripname : {
+        type: String,
+        required: true
+    },
+
+    tripduration :{
+        type: Number,
+        required: true
+    },
+
+    tripdistance: {
+        type: Number,
+        required: true
+    },
+
+    vehicleno: {
+        type: Number,
+        required: true
+    },
+
+    driverid: {
+        type: String,
+        required: true
+    },
+
+    startpoint: {
+        type: String,
+        required: true
+    },
+
+    destination: {
+        type: String,
+        required: true
+    },
+
+    tripgoods: {
+        type: String,
+        required: true
+    },
+
+    arrivaltime: {
+        type: Number,
+        required: true
+    },
+
+    departuretime: {
+        type: Number,
+        required: true
+    },
+
+})
+
+const trip = mongoose.model("trip", tripSchema)
+
+module.exports = trip;
