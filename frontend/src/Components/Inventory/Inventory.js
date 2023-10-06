630738103be90d89bd66f3fb2b162b0803639c07
Perera K C G S <it21810664@my.sliit.lk>
2023-10-07 02:21:54 +0530

    shift reorder level to upper level of the page. add the material UI dependencies.

--- frontend/src/Components/Inventory/Inventory.js
+++ frontend/src/Components/Inventory/Inventory.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import './Inventory.css';
+import './Inventory.css';
 
 const Inventory = () => {
   return (
-    <div className="inventory-container">
-      <div className="left-column">
+    <div className="inventory-containerg-10">
+      <div className="left-column-g11">
         <h2>Inventory Management System</h2>
-        <ul className="nav-list">
+        <ul className="nav-list-g12">
           <li>
-            <Link to="/inventory/addInventory" className="nav-link">
-              <button className="tab-button">Add New Item</button>
+            <Link to="/inventory/addInventory" className="nav-link-g13">
+              <button className="tab-button-g14">Add New Item</button>
             </Link>
           </li>
           <li>
-            <Link to="/inventory/allInventory" className="nav-link">
-              <button className="tab-button">Display All Items</button>
+            <Link to="/inventory/allInventory" className="nav-link-g13">
+              <button className="tab-button-g14">Display All Items</button>
             </Link>
           </li>
           <li>
-            <Link to="/inventory/updateInventory" className="nav-link">
-              <button className="tab-button">Update Item</button>
+            <Link to="/inventory/updateInventory" className="nav-link-g13">
+              <button className="tab-button-g14">Update Item</button>
             </Link>
           </li>
           <li>
-            <Link to="/inventory/uniqueInventory" className="nav-link">
-              <button className="tab-button">Display Unique Item</button>
+            <Link to="/inventory/uniqueInventory" className="nav-link-g13">
+              <button className="tab-button-g14">Display Unique Item</button>
             </Link>
           </li>
         </ul>
